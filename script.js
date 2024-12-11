bf82d3dc7b1cf97011470d86ca240c4dcf072098
Kerem Tutucu <Keremtutucu@gmail.com>
2024-12-11 15:11:24 +0100

    hamburger menu gefikst
    hamburger menu gefikst

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const menu = document.getElementById('menu');
+window.onload = function () {
+	window.addEventListener('scroll', function (e) {
+		if (window.pageYOffset > 100) {
+			document.querySelector("header").classList.add('is-scrolling');
+		} else {
+			document.querySelector("header").classList.remove('is-scrolling');
+		}
+	});
 
-    // Open of sluit het menu wanneer op het icoon wordt geklikt
-    hamburger.addEventListener('click', (event) => {
-        menu.classList.toggle('open');
-        event.stopPropagation(); // Voorkomt dat de klik ook het document-event triggert
-    });
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
 
-    // Sluit het menu wanneer ergens buiten het menu wordt geklikt
-    document.addEventListener('click', (event) => {
-        if (menu.classList.contains('open')) {
-            menu.classList.remove('open');
-        }
-    });
-});
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+}
